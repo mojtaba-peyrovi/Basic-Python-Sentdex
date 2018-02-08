571040855f9f3d265a85f78f575e7bcfeed124ba
= <mojtaba.peyrovi@gmail.com>
2018-02-08 11:42:26 +0700

    missing session 26 file added

---
+++ listManipulation-26.py
@@ -0,0 +1,20 @@
+x = [2,3,6,7,89,7,56,4,3,346,67,8]
+#x.append(2) # append 2 at the end
+
+#x.insert(2,999) # append 999 at index 2
+
+#x.remove(2) #removes the first 2
+
+#x.remove(x[2]) # removes the 3rd element of the list
+
+#print(x)
+#print(x[5:7]) # slices the data from index 5 to index 7 => result = 56,4
+#print(x[-1]) # prints the last element of the list
+#print(x.index(89)) # returns the index for the first value 89 in the list which is 4
+#print(x.count(3)) # shows how many 3 there is in the list
+
+x.sort() # sorts the list numerically
+print(x)
+y = ['Janet','Jessy','Kelly','Alice','Joe','Bob']
+y.sort() # sorts the list alphabetically
+print(y)
